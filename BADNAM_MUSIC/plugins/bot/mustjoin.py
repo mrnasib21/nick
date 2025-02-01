dcb2f4a01e8fb0c01a89bddb263eeb4308a0e509
Badnam xd <yadavxd746@gmail.com>
2025-02-01 14:06:17 +0530

    Update mustjoin.py

--- BADNAM_MUSIC/plugins/bot/mustjoin.py
+++ BADNAM_MUSIC/plugins/bot/mustjoin.py
@@ -27,7 +27,7 @@ async def must_join_channel(app: Client, msg: Message):
                         [
                             [
                                 InlineKeyboardButton("• ᴊᴏɪɴ •", url=link),
-                                InlineKeyboardButton("• ᴊᴏɪɴ •", url="https://t.me/ONE_WAS_SIGMA"),
+                                InlineKeyboardButton("• ᴊᴏɪɴ •", url="https://t.me/ABOUT_BADNAM_YADAV_XD"),
                             ]
                         ]
                     )
