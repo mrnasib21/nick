d39de7665fbd44d9d5adaf9435176fc7d7449335
mrnasib21 <mrnasib21@gmail.com>
2025-02-25 21:06:05 +0530

    Update admins.py

--- BADNAM_MUSIC/utils/decorators/admins.py
+++ BADNAM_MUSIC/utils/decorators/admins.py
@@ -44,7 +44,7 @@ def AdminRightsCheck(mystic):
                     [
                         InlineKeyboardButton(
                             text="ʜᴏᴡ ᴛᴏ ғɪx ?",
-                            callback_data="BADNAMmousAdmin",
+                            callback_data="NICKPAPAJI",
                         ),
                     ]
                 ]
@@ -140,7 +140,7 @@ def AdminActual(mystic):
                     [
                         InlineKeyboardButton(
                             text="ʜᴏᴡ ᴛᴏ ғɪx ?",
-                            callback_data="BADNAMmousAdmin",
+                            callback_data="NICKPAPAJI",
                         ),
                     ]
                 ]
