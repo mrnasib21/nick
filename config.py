12e07a8a9dcb93d227d28e010b14c9eba7ef6be4
mrnasib21 <mrnasib21@gmail.com>
2025-03-10 18:24:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -64,8 +64,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/WESTBENGALNETWORK")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/WESTBENGALNETWORK")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/NICKXMUSICROBOT" )
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/NICKXMUSICROBOT")
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
 
